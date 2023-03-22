dc5dbc5ff81cb409619f080e37cfa527fac357de
Lechorming <2842412433@qq.com>
2023-03-22 19:37:34 +0800

    lab3-1

--- lab3-1.py
+++ lab3-1.py
@@ -1,3 +1,13 @@
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
-img
+img=cv2.imread('Lenna.jpg')
+
+cv2.imshow('img1',img)
+
+hist=cv2.calcHist([img],[0],None,[256],[0,256])
+
+plt.figure(figsize=(10,8))
+plt.plot(hist)
+plt.show()
