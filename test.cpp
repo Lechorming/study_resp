010db201c375eb9565f99a24973f07c8d13493a5
Lechorming <2842412433@qq.com>
2023-04-12 16:25:25 +0800

    lab3

--- test.cpp
+++ test.cpp
@@ -1,13 +1,7 @@
-#include<cstring>
 #include<iostream>
 using namespace std;
 int main()
 {
-    char c[3];
-    const char a[]={'a','b','c'};
-    cout<<"输入\n";
-    for(int i;i<3;i++)
-    cin>>c[i];
-    cout<<strcmp(c,a)<<'\n';
+    cout<<"hello world\n";
     return 0;
 }
